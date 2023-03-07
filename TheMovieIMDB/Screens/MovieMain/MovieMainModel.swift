39da174500356f90ed07c061c28052ba910554e0
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2023-03-08 06:54:22 +0700

    add MovieMainModel

--- TheMovieIMDB/Screens/MovieMain/MovieMainModel.swift
+++ TheMovieIMDB/Screens/MovieMain/MovieMainModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct MovieResponse: Codable {
+	let results: [Movie]
+}
+
+struct Movie: Codable {
+	let id: Int
+	let mediaType: String?
+	let originalName: String?
+	let originalTitle: String?
+	let posterPath: String?
+	let overview: String?
+	let releaseDate: String?
+
+	enum CodingKeys: String, CodingKey {
+		case id
+		case mediaType = "media_type"
+		case originalName = "original_name"
+		case originalTitle = "original_title"
+		case posterPath = "poster_path"
+		case overview
+		case releaseDate = "release_date"
+	}
+}
