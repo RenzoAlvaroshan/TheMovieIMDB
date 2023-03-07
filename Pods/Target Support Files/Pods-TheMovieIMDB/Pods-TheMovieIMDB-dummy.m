36ac70b8b13ea7eff8a4d2093ec90ddd51ab82b3
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2023-03-07 16:18:57 +0700

    install pods

---
+++ Pods/Target Support Files/Pods-TheMovieIMDB/Pods-TheMovieIMDB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheMovieIMDB : NSObject
+@end
+@implementation PodsDummy_Pods_TheMovieIMDB
+@end
---
+++ Pods/Target Support Files/Pods-TheMovieIMDBTests/Pods-TheMovieIMDBTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheMovieIMDBTests : NSObject
+@end
+@implementation PodsDummy_Pods_TheMovieIMDBTests
+@end
