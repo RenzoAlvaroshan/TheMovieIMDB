db4c2756fc81d314fff0768b493b2497cdae64cd
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2023-03-08 06:55:23 +0700

    add MovieMainViewController

--- TheMovieIMDB/Base/SceneDelegate.swift
+++ TheMovieIMDB/Base/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		guard let scene = (scene as? UIWindowScene) else { return }
 		window = UIWindow(windowScene: scene)
 		window?.makeKeyAndVisible()
-		window?.rootViewController = UINavigationController(rootViewController: MovieMainViewController())
+		window?.rootViewController = UINavigationController(rootViewController: MovieMainViewController(viewModel: MovieMainViewModel()))
 	}
 
 	func sceneDidDisconnect(_ scene: UIScene) {
--- TheMovieIMDB/Screens/MovieMain/MovieMainViewController.swift
+++ TheMovieIMDB/Screens/MovieMain/MovieMainViewController.swift
@@ -6,23 +6,35 @@
 //
 
 import UIKit
+import IGListKit
 
 final class MovieMainViewController: UIViewController {
 	
 	// MARK: - Properties
 	
+	private let viewModel: MovieMainViewModel
+	
+	// MARK: - Initialization
+	
+	init(viewModel: MovieMainViewModel) {
+		self.viewModel = viewModel
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
 	// MARK: - Lifecycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		configureUI()
+		
 	}
 	
 	// MARK: - Selectors
 	
 	// MARK: - Helpers
 	
-	private func configureUI() {
-		view.backgroundColor = .systemRed
-	}
+	
 }
