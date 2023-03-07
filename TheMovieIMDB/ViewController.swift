c7f7774923b0a5462cb1e4fe6b320c531e146b58
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2023-03-07 15:56:47 +0700

    Initial Commit

---
+++ TheMovieIMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieIMDB
+//
+//  Created by Renzo Alvaroshan on 07/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
